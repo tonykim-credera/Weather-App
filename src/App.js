b062574036f4ae3c1e4ff29fafc8df00e9792207
Tony Kim <tony.kim@credera.com>
2021-09-30 14:46:03 -0500

    adding api through axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import Axios from 'axios' 
+
 function App() {
+
+  const cityName = "Dallas";
+  const apiKey = '31b837ade58a4fb0a1d191606213009';
+  const apiSource = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=5&aqi=no&alerts=no`;
+
+  const getData = () =>{
+    Axios.get(apiSource).then((response) => {
+      console.log(response);
+    })
+  }
+
   return (
-    <div> hello</div>
+    <div> Hello <button onClick={getData}> click me </button> </div>
   );
 }
 
